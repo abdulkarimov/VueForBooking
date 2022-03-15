8abf07e0d47cea23ad1f0f08cbde1510ce9766ff
ITstart <itstart@itstep.loc>
2022-03-15 21:47:19 +0600

    s

--- vue/src/views/CabinetById.vue
+++ vue/src/views/CabinetById.vue
@@ -7,7 +7,7 @@
   <div class="home" style="display: flex; justify-content: space-around; align-items: center;" >
     <div class="card" style="width: 18rem;">
     <div class="card-body">
-    <h5 class="card-title">{{cabinet.number_cabinet}}</h5>
+    <h5 class="card-title"> {{cabinet.number_cabinet}}</h5>
     <h6 class="card-subtitle mb-2 text-muted">{{cabinet.status}}</h6>
     <p class="card-text">{{cabinet.description}}</p>
   <input  type="date" id="start" name="trip-start" :value="this.date" :min ="this.date">
@@ -19,41 +19,38 @@
 
 <div v-show="flag1" >
 <div>
- <button type="button" id="1"  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">1:00</button>
- <button type="button" id="2"  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">2:00</button>
- <button type="button" id="3"  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">3:00</button>
- <button type="button" id="4"  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">4:00</button>
- <button type="button" id="5"  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">5:00</button>
- <button type="button" id="6"  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">6:00</button>
- <button type="button" id="7"  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">7:00</button>
- <button type="button" id="8"  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">8:00</button>
- <button type="button" id="9" ref="textName" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">9:00</button>
- <button type="button" id="10"  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">10:00</button>
- <button type="button" id="11"  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">11:00</button>
- <button type="button" id="12"  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">12:00</button>
+ <button type="button" @click='time("01:00")' :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">01:00</button>
+ <button type="button" @click='time("02:00")' :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">02:00</button>
+ <button type="button" @click='time("03:00")' :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">03:00</button>
+ <button type="button" @click='time("04:00")' :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">04:00</button>
+ <button type="button" @click='time("05:00")' :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">05:00</button>
+ <button type="button" @click='time("06:00")' :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">06:00</button>
+ <button type="button" @click='time("07:00")' :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">07:00</button>
+ <button type="button" @click='time("08:00")' :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">08:00</button>
+ <button type="button" @click='time("09:00")' :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">09:00</button>
+ <button type="button" @click='time("10:00")'   :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">10:00</button>
+ <button type="button" @click='time("11:00")'   :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">11:00</button>
+ <button type="button" @click='time("12:00")'   :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">12:00</button>
 </div>
 
 <div>
- <button type="button" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">13:00</button>
- <button type="button" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">14:00</button>
- <button type="button" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">15:00</button>
- <button type="button" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">16:00</button>
- <button type="button" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">17:00</button>
- <button type="button" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">18:00</button>
- <button type="button" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">19:00</button>
- <button type="button" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">20:00</button>
- <button type="button" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">21:00</button>
- <button type="button" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">22:00</button>
- <button type="button" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">23:00</button>
- <button type="button" :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">00:00</button>
+ <button type="button" @click='time("13:00")'  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">13:00</button>
+ <button type="button" @click='time("14:00")'  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">14:00</button>
+ <button type="button" @click='time("15:00")'  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">15:00</button>
+ <button type="button" @click='time("16:00")'  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">16:00</button>
+ <button type="button" @click='time("17:00")'  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">17:00</button>
+ <button type="button" @click='time("18:00")'  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">18:00</button>
+ <button type="button" @click='time("19:00")'  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">19:00</button>
+ <button type="button" @click='time("20:00")'  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">20:00</button>
+ <button type="button" @click='time("21:00")'  :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">21:00</button>
+ <button type="button" @click='time("22:00")' :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">22:00</button>
+ <button type="button" @click='time("23:00")' :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">23:00</button>
+ <button type="button" @click='time("00:00")' :style="{ color: activeColor}" class="btn btn-light" style="margin: 10px;">00:00</button>
 </div>
 
 
-<button type="button" class="btn btn-primary"  @click='Click()'>Apply </button>
+<button type="button" class="btn btn-primary"  @click='Push()'>Apply</button>
 </div>
-
-
-
 
   </div>  
   </body>
@@ -71,33 +68,54 @@ export default {
     cabinet: {},
     date : "",
     booking: {},
+    startTime: "",
+    endTime : ""
   }
 },
  methods : {
 Click() {
+      // axios.post('http://abdula.loc/api/booking/test', {
+      //     "id" : this.$route.params.id,
+      //     "time" : this.date
+      // }).then(response => (this.booking = response.data))
+
+    //   .then(response => (this.booking = response.data.forEach(function(data, index) {
+    //     var start = data['time_start'].split(" ");    
+    //     var end = data['time_end'].split(" ");  
+    // })));
     this.flag1 = true;
-      axios.post('http://abdula.loc/api/booking/test', {
-          "id" : this.$route.params.id,
-          "time" : this.date
+    },
+    Push(){
+      axios.post('http://abdula.loc/api/booking', {
+        "user_id" : 1,
+        "cabinet_id" : this.$route.params.id,
+          "time_start" : this.date +' '+this.startTime,
+          "time_end" : this.date +' '+this.endTime,
       })
-      .then(response => (this.booking = response.data.forEach(function(data, index) {
-        var start = data['time_start'].split(" ");    
-        var end = data['time_end'].split(" ");    
+      .then(response => (this.cabinet = response.data));
+    },
+    time(string){
 
-        
+      this.counted = this.counted + 1
+      if (this.counted == 1)
+      {
+        this.startTime = string;
+        this.counted = this.counted + 1 
+      }
+      else if (this.counted == 3)
+      {
+        this.endTime = string;
+        this.counted = this.counted + 1 
+      }
+      if (this.counted == 5)
+          {
+            this.counted = 2
+            this.startTime = string;
 
+          }
 
-  
-         // for(var i = 0; i < minus; i++)
-      
+    }
 
-
-    })));
-
-
-
-
-      },
     },
       mounted() {
        var d = new Date();
